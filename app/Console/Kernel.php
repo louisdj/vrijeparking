588705d5baeb5fe060be0750e9fe377e9b6956fa
robbertgoeminne <robbert.goeminne@student.howest.be>
2016-03-23 22:29:48 +0100

    Inleidende code voor cronjobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use App\Parking;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+
+        //Update parkings
         $schedule->call(function () {
             $json = file_get_contents('http://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json');
             $gent = json_decode($json);
@@ -32,11 +35,12 @@ class Kernel extends ConsoleKernel
             foreach($gent as $parking)
             {
                 Parking::where('naam', strtolower($parking->description))->update(['beschikbare_plaatsen' => $parking->parkingStatus->availableCapacity]);
+                $id = DB::table('parkings')->select('id')->where('naam', strtolower($parking->description))->get();
+
+                DB::table('parkings_historie')->insert([
+                    ['parking_id' => $id[0]->id, 'bezetting' => $parking->parkingStatus->availableCapacity]
+                ]);
             }
         })->everyMinute();
-
-
-//        $schedule->command('inspire')->hourly();
-//        $schedule->command('pullParkingData')->everyMinute();
     }
 }
