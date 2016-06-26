509a251c98fcf5c398f86712416a08dfd66b4cb4
matthieucalie <matthieu.calie@wijs.be>
2016-06-26 11:58:56 +0200

    Added authentication for backend

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,28 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
 Route::group(['middleware' => 'web'], function () {
 
     Route::get('/', function () {
@@ -51,7 +28,21 @@ Route::group(['middleware' => 'web'], function () {
         return View::make('home');
     });
 
-    Route::get('/beheer', 'ManagementController@index');
+//    Route::get('/beheer', 'ManagementController@index');
+
+    // Authentication Routes...
+    $this->get('login', 'Auth\AuthController@showLoginForm');
+    $this->post('login', 'Auth\AuthController@login');
+    $this->get('logout', 'Auth\AuthController@logout');
+
+    // Registration Routes... disabled for now
+//    $this->get('register', 'Auth\AuthController@showRegistrationForm');
+//    $this->post('register', 'Auth\AuthController@register');
+
+    Route::group(['as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth'], 'prefix' => 'beheer'], function() {
+        Route::get('/', 'HomeController@index');
+    });
+
 
 });
 
