f5312e625577e2b3b1f374d0f247e4e4dea9d241
robbertgoeminne <robbert.goeminne@student.howest.be>
2017-04-20 19:03:58 +0200

    Mindervaliden nog niet zichtbaar maken

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -111,7 +111,7 @@
                     </li>
 
                     <li>
-                        <a href="/mindervaliden"><i class="fa fa-wheelchair" aria-hidden="true"></i> Voorbehouden parking</a>
+                        {{--<a href="/mindervaliden"><i class="fa fa-wheelchair" aria-hidden="true"></i> Voorbehouden parking</a>--}}
                     </li>
 
                     <li class="page-scroll">
@@ -151,7 +151,7 @@
 
     @yield('content')
 
-    @if(isset($nofooter) && $nofooter == 'true')
+    @if(!isset($nofooter))
     <!-- Footer -->
     <footer class="text-center">
         <div class="footer-above">
@@ -181,7 +181,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        Copyright &copy; VrijeParking.be 2016 <br/>
+                        Copyright &copy; VrijeParking.be {{ date('Y') }} <br/>
                         <a HREF="mailto:Contact@vrijeparking.be">Contact@vrijeparking.be</a>
                     </div>
                 </div>
