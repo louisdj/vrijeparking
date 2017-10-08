3585e72d08a7a7970650210ed58576d513a0a2d6
robbertgoeminne <robbert.goeminne@student.howest.be>
2017-10-08 21:30:53 +0200

    Login doel vervangen

--- resources/views/embed/index.blade.php
+++ resources/views/embed/index.blade.php
@@ -92,7 +92,7 @@ a:hover {
 }
 
 
-@media (max-width: 800px) {
+@media (max-width: 1100px) {
     #sidebar {
         width: auto;
         position: relative;
@@ -103,7 +103,7 @@ a:hover {
 
 }
 
-@media (max-width: 600px) {
+@media (max-width: 1100px) {
     #menubar {
         width: auto;
         float: none;
@@ -115,249 +115,15 @@ a:hover {
 </style>
 
 <style>/* Stylesheet 2: */
-body {
-    font-family: Arial;
-    background-color: #d14836;
-    line-height: 20px;
-}
 
-.container {
-    xmin-width: 900px;
-}
-
-.wrapper {
-    position: relative;
-    overflow: auto;
-}
-
-#top {
-    color: #ffffff;
-    padding: 15px;
-    font-size: 30px;
-    line-height: 26px;
-}
-
-#top h1 {
-    margin:0;
-    line-height: 50px;
-}
-
-#menubar {
-    width: 190px;
-    float: right;
-}
-
-#main {
-    padding: 10px;
-    background-color: #ffffff;
-    font: 80% Verdana;
-}
-
-#main h1, #main h2 {
-    color: #d14836;
-}
-
-#sidebar {
-    background-color: #F6DAD7;
-    color: #d14836;
-    padding: 10px;
-}
-
-#bottom {
-    text-align: center;
-    padding: 10px;
-    font-size: 70%;
-    color: #ffffff;
-}
-
-#menulist {
-    padding:0;
-    font: 16px verdana;
-}
-
-.menuitem {
-    width: 155px;
-    background-color: #d14836;
-    border: 1px solid #d14836;
-    border-radius: 40px;
-    color: #ffffff;
-    list-style-type: none;
-    margin: 10px;
-    padding: 5px;
-    text-align: center;
-    cursor: pointer;
-}
-
-.menuitem:nth-child(2) {
-    background-color:white;
-    color: #d14836;
-    font-weight:bold;
-}
-
-.menuitem:hover {
-    background-color: #ffffff;
-    color: #d14836;
-}
-
-a {
-    color: #d14836;
-    text-decoration: none;
-}
-
-a:hover {
-    text-decoration: underline;
-}
 </style>
 
 <style>/* Stylesheet 3: */
-body {
-    font: 100% Verdana;
-    margin: 20px;
-    line-height: 26px;
-}
 
-.container {
-    xmin-width: 900px;
-}
-
-.wrapper {
-    position: relative;
-    overflow: auto;
-}
-
-#sidebar {
-    background-color: #f1f1f1;
-    border: 1px solid #d4d4d4;
-    padding-left: 10px;
-}
-
-#bottom {
-    text-align: center;
-    padding: 10px;
-    font-size: 70%;
-    line-height: 14px;
-}
-
-h1, h2, h3 {
-    color: #4CAF50;
-}
-
-#menulist {
-    padding: 0;
-    position: relative;
-    overflow: auto;
-}
-
-.menuitem {
-    width: 165px;
-    float: left;
-    background-color: #555555;
-    color: #ffffff;
-    list-style-type: none;
-    margin: 4px;
-    padding: 2px;
-    text-align: center;
-    cursor: pointer;
-}
-
-.menuitem:nth-child(3) {
-    background-color:#4CAF50;
-}
-
-.menuitem:hover {
-    background-color: #999999;
-}
-
-a {
-    color: #000000;
-}
-
-a:hover {
-    color: #84c754;
-}
 </style>
 <style>/* Stylesheet 4: */
-body {
-    font: 100% Courier New;
-    margin: 20px;
-    line-height: 26px;
-    background-color: #000000;
-}
 
-.container {
-    xmin-width: 900px;
-}
 
-.wrapper {
-    position: relative;
-    overflow: auto;
-}
-
-#top {
-    color: #84c754;
-    padding: 15px;
-}
-
-#main {
-    padding: 10px;
-    color: #84c754;
-}
-
-#sidebar {
-    color: #ffffff;
-    border: 1px solid #ffffff;
-    border-radius: 4px;
-    padding: 10px;
-    width: 320px;
-    top: 0;
-    right: 0;
-    position: absolute;
-    font-size: 80%;
-    line-height: 20px;
-}
-
-#bottom {
-    border: 1px solid #ffffff;
-    border-radius: 4px;
-    color: #ffffff;
-    text-align: center;
-    padding: 10px;
-    font-size: 70%;
-    line-height: 14px;
-}
-
-#top h1,#top p {
-    margin: 0;
-}
-
-.menuitem {
-    color: #84c754;
-    cursor: pointer;
-}
-
-.menuitem:nth-child(4) {
-    color:white;
-    font-weight:bold;
-}
-
-.menuitem:hover {
-    color: #ffffff;
-}
-
-a {
-    color: #ffffff;
-}
-
-a:hover {
-    color: #84c754;
-}
-@media (max-width: 600px) {
-    #sidebar {
-        width: auto;
-        margin-bottom:10px;
-        position: relative;
-    }
-}
 
 </style>
 
